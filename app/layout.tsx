6ef9f6e7af24c72a71e60d923f2e5ae727cb7f89
BrenoSilveiraLeal <brenosilveiraleal@gmail.com>
2026-09-01 01:45:05 -0300

    Refactor code structure for improved readability and maintainability

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ export const metadata: Metadata = {
   title: "Casa da Inovação E. F. Therezópolis",
   description: "Tecnologia, cultura e oportunidades gratuitas em Teresópolis.",
   icons: {
-    icon: "/imagens/logo-casa-nova-rounded.png",
-    shortcut: "/imagens/logo-casa-nova-rounded.png",
+    icon: "/imagens/otimizadas/favicon.png",
+    shortcut: "/imagens/otimizadas/favicon.png",
   },
 };
 
